3d7913c59afe7c7c6e978ddb799389fefe323d09
prakharsharma13 <prakharsharma@752gmail.com>
2025-05-26 00:44:49 +0530

    string methods

---
+++ stringMethods.js
@@ -0,0 +1,17 @@
+let myName = "prakhar";
+
+//slice
+let slicedName = myName.slice(0, 3);
+console.log(slicedName);
+
+//replace
+let replacedName = myName.replace("pra", "PRA");
+console.log(replacedName);
+
+//concat
+let fullName = myName.concat(" ", "sharma");
+console.log(fullName);
+
+//trim
+let trimmedName = myName.trim();
+console.log(trimmedName);
